75406eb9a3ea81e36291a2c5dd78b41db85555a7
Yukislayers <65172701+Yukislayers@users.noreply.github.com>
2022-11-26 16:21:51 +0100

    New changes added

--- Test/Keygen.py
+++ Test/Keygen.py
@@ -1,7 +1,17 @@
 import Primal
+import os
 
 #Generate a 2048 bit prime number : p
-p = Primal.primal()
+#We will store this number in a file so we don't do it every time
+
+if not (os.path.isfile('Test/primal.txt')):
+    p = Primal.primal()
+    file = open("Test/primal.txt", "w")
+    file.write(str(p))
+    file.close()
+
+f = open("Test/primal.txt", "r")
+p = int(f.read())
 print("2048 bit prime p is :",p)
 print("p has for bit length : ", p.bit_length())
 
@@ -14,7 +24,9 @@ print("p has for bit length : ", p.bit_length())
 #Si j'utilise la fonction, j'ai un probleme de memoire
 #Il faut trouver une autre methode que celle en dessous 
 #meme si elle fonctionne pour des petits nombres
-def generator(n):
+
+'''
+def generator(p):
     size = set(range(1, n))
     results = []
     for a in size:
@@ -24,5 +36,6 @@ def generator(n):
             if g == size:
                 results.append(a)
     return results
+'''
 
 #print(f"Z_p has generators {gen}")
