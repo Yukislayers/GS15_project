43ad707f3b11e1a01ec18f69ad51606b97e68096
Yukislayers <65172701+Yukislayers@users.noreply.github.com>
2022-11-26 15:18:02 +0100

    New changes

--- Test/Keygen.py
+++ Test/Keygen.py
@@ -7,7 +7,13 @@ print("p has for bit length : ", p.bit_length())
 
 #Now that we got P
 #We want to find a generator element of Zp
+
+
 #This function gives us all the generator of n
+
+#Si j'utilise la fonction, j'ai un probleme de memoire
+#Il faut trouver une autre methode que celle en dessous 
+#meme si elle fonctionne pour des petits nombres
 def generator(n):
     size = set(range(1, n))
     results = []
